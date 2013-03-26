23be94c6fc409f935afeee013ba9135336ce7c83
huang he <huanghe@huangmatoMacBook-Pro.local>
2013-03-26 23:43:26 +0800

    add

---
+++ SanGuoFruit/SanGuoFruit/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  SanGuoFruit
+//
+//  Created by huang he on 13-3-19.
+//  Copyright huang he 2013年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ SanGuoFruit/SanGuoFruit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SanGuoFruit
+//
+//  Created by huang he on 13-3-19.
+//  Copyright huang he 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
